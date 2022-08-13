29c29fb50f34c07f15a5e5628cd67e633d8a5e0b
Alew <xalejandrow@gmail.com>
2022-08-13 00:03:21 +0000

    empezando ejercicio streamliy

--- appy2.py
+++ appy2.py
@@ -11,3 +11,10 @@ df_raw.info()
 
 X = df.drop(['total_cup_points','specialty'], axis=1)
 y = df['specialty']
+
+
+#PASSENGER =  {1: "1st", 2: "2nd", 3: "3rd"}
+#def format_func_passenger(option):
+#   return PASSENGER[option]
+#
+#pclass = st.sidebar.selectbox("Passenger Class" ,options=list(PASSENGER.keys()), format_func=format_func_passenger)
